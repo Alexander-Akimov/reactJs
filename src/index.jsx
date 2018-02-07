d67ddb9c3c18a3ef90fbb1d36df80f3c02a96ae3
Alexander <a.akimov@dostek.kg>
2018-02-07 18:16:04 +0600

    Event logging

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Article from './Article'
+import ArticleList from './ArticleList'
 import {articles} from './fixtures'
 // function HelloWorld() {
 // 	return <h1>Hello World</h1>
 // }
 
-render(<Article article = {articles[0]} /* foo = 'bar'*//>, document.getElementById('container'));
+// render(<Article article = {articles[0]} /* foo = 'bar'*//>, document.getElementById('container'));
+render(<ArticleList articles = {articles} />, document.getElementById('container'));
